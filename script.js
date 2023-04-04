75877a3c5a84c3094e26e331f5378644376803d1
nshutidavid <nshutitoto@gmail.com>
2023-04-04 17:51:34 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,93 @@
+const resultEl = document.getElementById('result');
+const lenghtEl = document.getElementById('lenght');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
+
+clipboardEl.addEventListener('click', () => {
+  const textarea = document.createElement('textarea');
+  const password = resultEl.innerText;
+
+  if (!password) {
+    return;
+  }
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  textarea.remove();
+  alert('Password copied to clipboard');
+});
+
+generateEl.addEventListener('click', () => {
+  const lenght = +lenghtEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  resultEl.innerText = generatePassword(
+    hasLower,
+    hasNumber,
+    hasSymbol,
+    hasUpper,
+    lenght
+  );
+});
+
+function generatePassword(upper, lower, number, symbol, lenght) {
+  let generatedPassword = '';
+  const typesCount = upper + lower + number + symbol;
+  const typesArr = [{ upper }, { lower }, { number }, { symbol }].filter(
+    (item) => Object.values(item)[0]
+  );
+
+  if (typesCount === 0) {
+    return '';
+  }
+
+  for (let i = 0; i < lenght; i += typesCount) {
+    typesArr.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+  const finalPassword = generatedPassword.slice(0, lenght);
+
+  return finalPassword;
+}
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*(){}=<>/,.?';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+//check on w3schools for fromCharCode
+/*console.log(
+  getRandomLower(),
+  getRandomUpper(),
+  getRandomNumber(),
+  getRandomSymbol()
+);
+*/
